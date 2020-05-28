7cf2d3dba57088cbb6fa3265400dc6f459158868
Akshat Joshi <akshatjoshi375@gmail.com>
2020-05-28 20:54:42 +0530

    new website

---
+++ precache-manifest.af919c1360847306e3be5819c9bbb83c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec95b3826e45bca6fa354e674003af79",
+    "url": "./index.html"
+  },
+  {
+    "revision": "af3286824a33303e9ffb",
+    "url": "./static/css/main.0de458d0.chunk.css"
+  },
+  {
+    "revision": "30a321ad43928ab688c9",
+    "url": "./static/js/2.71e6f0db.chunk.js"
+  },
+  {
+    "revision": "ddb934a41eb6bf13706c65fe3758fb10",
+    "url": "./static/js/2.71e6f0db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af3286824a33303e9ffb",
+    "url": "./static/js/main.aec7ab92.chunk.js"
+  },
+  {
+    "revision": "fdc9e4a82c36edc0fa67",
+    "url": "./static/js/runtime-main.bd01b662.js"
+  },
+  {
+    "revision": "2387577670b652cd986933ab0fb81f78",
+    "url": "./static/media/about.23875776.PNG"
+  }
+]);
---
+++ precache-manifest.f9d703f1486e7bbe8262a8d87a19a164.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8d04d85cd324c1767ecd325cf11265f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4e352f2391a0c2f2cff2",
+    "url": "/static/css/main.0de458d0.chunk.css"
+  },
+  {
+    "revision": "30a321ad43928ab688c9",
+    "url": "/static/js/2.71e6f0db.chunk.js"
+  },
+  {
+    "revision": "ddb934a41eb6bf13706c65fe3758fb10",
+    "url": "/static/js/2.71e6f0db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e352f2391a0c2f2cff2",
+    "url": "/static/js/main.ada9320b.chunk.js"
+  },
+  {
+    "revision": "4ac598fa20d7619937c8",
+    "url": "/static/js/runtime-main.90834230.js"
+  },
+  {
+    "revision": "2387577670b652cd986933ab0fb81f78",
+    "url": "/static/media/about.23875776.PNG"
+  }
+]);
